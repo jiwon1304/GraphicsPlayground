37e2d2dd6214dece2293f2a9d43643833a5885b2
kbjun98 <110660221+kbjun98@users.noreply.github.com>
2025-05-18 01:18:54 +0900

    feat: UnrealEd에 ParticleViewerPanel 추가, Render와 OnResize에서 WindowType별로 처리

--- EngineSIU/EngineSIU/Engine/Source/Editor/UnrealEd/UnrealEd.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Editor/UnrealEd/UnrealEd.cpp
@@ -5,63 +5,82 @@
 #include "PropertyEditor/OutlinerEditorPanel.h"
 #include "PropertyEditor/PropertyEditorPanel.h"
 #include "PropertyEditor/SkeletalMeshViewerPanel.h"
+#include "PropertyEditor/SubEditor/ParticleViewerPanel.h"
 #include "World/World.h"
 void UnrealEd::Initialize()
 {
     auto ControlPanel = std::make_shared<ControlEditorPanel>();
     Panels["ControlPanel"] = ControlPanel;
-    
+
     auto OutlinerPanel = std::make_shared<OutlinerEditorPanel>();
     Panels["OutlinerPanel"] = OutlinerPanel;
-    
+
     auto PropertyPanel = std::make_shared<PropertyEditorPanel>();
     Panels["PropertyPanel"] = PropertyPanel;
 
     // TODO : SkeletalViewe 전용 UI 분리
     auto BoneHierarchyPanel = std::make_shared<SkeletalMeshViewerPanel>();
     Panels["BoneHierarchyPaenl"] = BoneHierarchyPanel;
+
+    auto ParticlePanel = std::make_shared<ParticleViewerPanel>();
+    ParticlePanel->WindowType = WT_ParticleSubWindow;
+    ParticleSubPanels["ParticleViewerPanel"] = ParticlePanel;
 }
 
-void UnrealEd::Render() const
+void UnrealEd::Render(EWindowType WindowType) const
 {
-    EWorldTypeBitFlag currentMask;
-    switch (GEngine->ActiveWorld->WorldType)
+    if (WindowType == EWindowType::WT_Main)
     {
-    case EWorldType::Game:
-        currentMask = EWorldTypeBitFlag::Game;
-        break;
-    case EWorldType::Editor:
-        currentMask = EWorldTypeBitFlag::Editor;
-        break;
-    case EWorldType::PIE:
-        currentMask = EWorldTypeBitFlag::PIE;
-        break;
-    case EWorldType::EditorPreview:
-        currentMask = EWorldTypeBitFlag::EditorPreview;
-        break;
-    case EWorldType::GamePreview:
-        currentMask = EWorldTypeBitFlag::GamePreview;
-        break;
-    case EWorldType::GameRPC:
-        currentMask = EWorldTypeBitFlag::GameRPC;
-        break;
-    case EWorldType::SkeletalViewer:
-        currentMask = EWorldTypeBitFlag::SkeletalViewer;
-        break;
-    case EWorldType::Inactive:
-        currentMask = EWorldTypeBitFlag::Inactive;
-        break;
-    default:
-        currentMask = EWorldTypeBitFlag::None;
-        break;
-        
-    }
-    for (const auto& Panel : Panels)
-    {
-        if (HasFlag(Panel.Value->GetSupportedWorldTypes(), currentMask))
+        EWorldTypeBitFlag currentMask;
+        switch (GEngine->ActiveWorld->WorldType)
         {
-            Panel.Value->Render();
+        case EWorldType::Game:
+            currentMask = EWorldTypeBitFlag::Game;
+            break;
+        case EWorldType::Editor:
+            currentMask = EWorldTypeBitFlag::Editor;
+            break;
+        case EWorldType::PIE:
+            currentMask = EWorldTypeBitFlag::PIE;
+            break;
+        case EWorldType::EditorPreview:
+            currentMask = EWorldTypeBitFlag::EditorPreview;
+            break;
+        case EWorldType::GamePreview:
+            currentMask = EWorldTypeBitFlag::GamePreview;
+            break;
+        case EWorldType::GameRPC:
+            currentMask = EWorldTypeBitFlag::GameRPC;
+            break;
+        case EWorldType::SkeletalViewer:
+            currentMask = EWorldTypeBitFlag::SkeletalViewer;
+            break;
+        case EWorldType::Inactive:
+            currentMask = EWorldTypeBitFlag::Inactive;
+            break;
+        default:
+            currentMask = EWorldTypeBitFlag::None;
+            break;
+
         }
+        for (const auto& Panel : Panels)
+        {
+            if (HasFlag(Panel.Value->GetSupportedWorldTypes(), currentMask))
+            {
+                Panel.Value->Render();
+            }
+        }
+    }
+    else if (WindowType == EWindowType::WT_ParticleSubWindow)
+    {
+        for (const auto& Panel : ParticleSubPanels)
+        {
+            if (Panel.Value)
+            {
+                Panel.Value->Render();
+            }
+        }
+        return;
     }
 }
 
@@ -70,11 +89,27 @@ void UnrealEd::AddEditorPanel(const FString& PanelId, const std::shared_ptr<UEdi
     Panels[PanelId] = EditorPanel;
 }
 
-void UnrealEd::OnResize(HWND hWnd) const
+void UnrealEd::OnResize(HWND hWnd, EWindowType WindowType) const
 {
-    for (auto& Panel : Panels)
+    if (WindowType == EWindowType::WT_Main)
     {
-        Panel.Value->OnResize(hWnd);
+        for (auto& Panel : Panels)
+        {
+            if (Panel.Value)
+            {
+                Panel.Value->OnResize(hWnd);
+            }
+        }
+    }
+    else if (WindowType == EWindowType::WT_ParticleSubWindow)
+    {
+        for (auto& Panel : ParticleSubPanels)
+        {
+            if (Panel.Value)
+            {
+                Panel.Value->OnResize(hWnd);
+            }
+        }
     }
 }
 
--- EngineSIU/EngineSIU/Engine/Source/Editor/UnrealEd/UnrealEd.h
+++ EngineSIU/EngineSIU/Engine/Source/Editor/UnrealEd/UnrealEd.h
@@ -1,6 +1,7 @@
-﻿#pragma once
+#pragma once
 #include "Container/Map.h"
 #include "Container/String.h"
+#include "EditorPanel.h"
 
 class UEditorPanel;
 
@@ -10,13 +11,14 @@ public:
     UnrealEd() = default;
     ~UnrealEd() = default;
     void Initialize();
-    
-     void Render() const;
-     void OnResize(HWND hWnd) const;
+
+    void Render(EWindowType WindowType = EWindowType::WT_Main) const;
+     void OnResize(HWND hWnd, EWindowType WindowType = EWindowType::WT_Main) const;
     
     void AddEditorPanel(const FString& PanelId, const std::shared_ptr<UEditorPanel>& EditorPanel);
     std::shared_ptr<UEditorPanel> GetEditorPanel(const FString& PanelId);
 
 private:
     TMap<FString, std::shared_ptr<UEditorPanel>> Panels;
+    TMap<FString, std::shared_ptr<UEditorPanel>> ParticleSubPanels;
 };
