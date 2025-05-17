74de6255addfc654256046f2c8c9cdeebb990ea8
kbjun98 <110660221+kbjun98@users.noreply.github.com>
2025-05-17 22:35:07 +0900

    feat: ParticleSubEngine 이식

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Windows/SubWindow/ParticleSubEngine.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "SubEngine.h"
+
+class UStaticMeshComponent;
+class UParticleSystemSubEngine : public USubEngine
+{
+    DECLARE_CLASS(UParticleSystemSubEngine, USubEngine)
+public:
+    UParticleSystemSubEngine();
+    ~UParticleSystemSubEngine();
+public:
+    virtual void Initialize(HWND& hWnd, FGraphicsDevice* InGraphics, FDXDBufferManager* InBufferManager, UImGuiManager* InSubWindow, UnrealEd* InUnrealEd);
+    virtual void Tick(float DeltaTime);
+    virtual void Input(float DeltaTime);
+    virtual void Render();
+    virtual void Release();
+    UStaticMeshComponent* UnrealSphereComponent = nullptr;
+};
+
