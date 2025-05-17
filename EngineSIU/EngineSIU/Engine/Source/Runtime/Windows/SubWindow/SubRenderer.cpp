522001a7bb94046ede299d27759302807a84739c
kbjun98 <110660221+kbjun98@users.noreply.github.com>
2025-05-18 01:58:42 +0900

    feat: SubRenderer의 Prepare, Initialize추가.

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Windows/SubWindow/SubRenderer.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Windows/SubWindow/SubRenderer.cpp
@@ -1,1 +1,53 @@
 #include "SubRenderer.h"
+#include "UnrealEd/EditorViewportClient.h"
+#include "Engine/UnrealClient.h"
+
+void FSubRenderer::Initialize(FGraphicsDevice* InGraphics, FDXDBufferManager* InBufferManager, USubEngine* InEngine)
+{
+    Engine = InEngine;
+    Graphics = InGraphics;
+    BufferManager = InBufferManager;
+}
+
+void FSubRenderer::PrepareRender(FEditorViewportClient* Viewport)
+{
+    const EViewModeIndex ViewMode = Viewport->GetViewMode();
+    TargetViewport = Viewport;
+    //UpdateViewCamera(Viewport);
+    FViewportResource* ViewportResource = Viewport->GetViewportResource();
+    FRenderTargetRHI* RenderTargetRHI = ViewportResource->GetRenderTarget(EResourceType::ERT_Scene);
+    FDepthStencilRHI* DepthStencilRHI = ViewportResource->GetDepthStencil(EResourceType::ERT_Scene);
+
+    Graphics->DeviceContext->OMSetRenderTargets(0, nullptr, nullptr);
+    
+    Graphics->DeviceContext->ClearRenderTargetView(
+        RenderTargetRHI->RTV,
+        Graphics->ClearColor
+    );
+
+    Graphics->DeviceContext->ClearDepthStencilView(
+        Graphics->DeviceDSV,
+        D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL,
+        1.0f, 0
+    );
+
+    Graphics->DeviceContext->OMSetRenderTargets(
+        1,
+        &RenderTargetRHI->RTV,
+        nullptr
+    );
+
+    if (Viewport->GetViewMode() == EViewModeIndex::VMI_Wireframe)
+        Graphics->DeviceContext->RSSetState(Graphics->RasterizerWireframeBack);
+    else
+        Graphics->DeviceContext->RSSetState(Graphics->RasterizerSolidBack);
+
+    Graphics->DeviceContext->OMSetDepthStencilState(
+        Graphics->DepthStencilState,
+        0);
+
+}
+
+void FSubRenderer::Render()
+{
+}
--- EngineSIU/EngineSIU/Engine/Source/Runtime/Windows/SubWindow/SubRenderer.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Windows/SubWindow/SubRenderer.h
@@ -1,5 +1,22 @@
 #pragma once
+
+class FGraphicsDevice;
+class USubEngine;
+class FDXDBufferManager;
+class FEditorViewportClient;
+
 class FSubRenderer
 {
+public:
+
+    void Initialize(FGraphicsDevice* InGraphics, FDXDBufferManager* InBufferManager, USubEngine* InEngine);
+    void PrepareRender(FEditorViewportClient* Viewport);
+    void Render();
+private:
+
+    FGraphicsDevice* Graphics;
+    USubEngine* Engine = nullptr;
+    FDXDBufferManager* BufferManager;
+    FEditorViewportClient* TargetViewport = nullptr;
 };
 
