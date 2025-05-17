92dc1a57188b09eed39932dd63ce6cfc39542962
kbjun98 <110660221+kbjun98@users.noreply.github.com>
2025-05-17 21:04:17 +0900

    feat: SubEngine, ImGuiSubWindow 이식
    imgui 폰트 추가

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Windows/SubWindow/SubEngine.cpp
@@ -0,0 +1,65 @@
+#include "SubEngine.h"
+
+#include "ImGuiManager.h"
+#include "ImGuiSubWindow.h"
+#include "SubRenderer.h"
+#include "UnrealClient.h"
+#include "UnrealEd/EditorViewportClient.h"
+
+USubEngine::USubEngine() :
+    Graphics(nullptr),
+    BufferManager(nullptr),
+    Wnd(nullptr),
+    SubRenderer(nullptr),
+    UnrealEditor(nullptr),
+    SubUI(nullptr),
+    bIsShowSubWindow(false),
+    EditorPlayer(nullptr)
+{
+}
+
+USubEngine::~USubEngine()
+{
+}
+
+void USubEngine::Initialize(HWND& hWnd, FGraphicsDevice* InGraphics, FDXDBufferManager* InBufferManager, UImGuiManager* InSubWindow, UnrealEd* InUnrealEd)
+{
+    //Graphics = InGraphics;
+    //BufferManager = InBufferManager;
+    //Wnd = &hWnd;
+    //SubRenderer = new FSubRenderer;
+    //UnrealEditor = InUnrealEd;
+    //SubUI = new FImGuiSubWindow(hWnd, InGraphics->Device, InGraphics->DeviceContext);
+    //UImGuiManager::ApplySharedStyle(InSubWindow->GetContext(), SubUI->Context);
+    //SubRenderer->Initialize(InGraphics, InBufferManager, this);
+
+    //ViewportClient = new FEditorViewportClient();
+    //ViewportClient->Initialize(EViewScreenLocation::EVL_MAX, FRect(0, 0, 800, 600), this);
+    //ViewportClient->CameraReset();
+    //ViewportClient->FarClip = 1000000;
+
+}
+
+void USubEngine::Input(float DeltaTime)
+{
+
+}
+
+void USubEngine::Tick(float DeltaTime)
+{
+}
+
+void USubEngine::Render()
+{
+
+}
+
+void USubEngine::Release()
+{
+}
+
+void USubEngine::RequestShowWindow(bool bShow)
+{
+    bIsShowSubWindow = bShow;
+    bIsShowing = bShow;
+}
