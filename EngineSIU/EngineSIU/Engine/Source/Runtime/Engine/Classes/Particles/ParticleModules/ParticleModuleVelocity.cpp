ca5f2951a3442bf9b1555a8bc8a181dbfbac1579
gudtldn <gudtldn1234@gmail.com>
2025-05-20 17:37:47 +0900

    feat: ParticleModuleVelocity 및 ParticleModuleVelocityBase 클래스 추가 및 vcxproj 파일에 포함

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Particles/ParticleModules/ParticleModuleVelocity.cpp
@@ -0,0 +1,80 @@
+﻿#include "ParticleModuleVelocity.h"
+
+#include "ParticleModuleRequired.h"
+#include "Components/ParticleSystemComponent.h"
+#include "Distributions/DistributionFloatUniform.h"
+#include "Distributions/DistributionVectorUniform.h"
+#include "Particles/ParticleEmitter.h"
+#include "Particles/ParticleEmitterInstances.h"
+#include "Particles/ParticleHelper.h"
+#include "Particles/ParticleLODLevel.h"
+#include "UObject/ObjectFactory.h"
+
+UParticleModuleVelocity::UParticleModuleVelocity()
+{
+    bSpawnModule = true;
+}
+
+void UParticleModuleVelocity::InitializeDefaults()
+{
+    if (!StartVelocity.IsCreated())
+    {
+        StartVelocity.Distribution = FObjectFactory::ConstructObject<UDistributionVectorUniform>(this, TEXT("DistributionStartVelocity"));
+    }
+
+    if (!StartVelocityRadial.IsCreated())
+    {
+        StartVelocityRadial.Distribution = FObjectFactory::ConstructObject<UDistributionFloatUniform>(this, TEXT("DistributionStartVelocityRadial"));
+    }
+}
+
+void UParticleModuleVelocity::PostInitProperties()
+{
+    Super::PostInitProperties();
+    InitializeDefaults();
+}
+
+void UParticleModuleVelocity::Spawn(FParticleEmitterInstance* Owner, int32 Offset, float SpawnTime, FBaseParticle* ParticleBase)
+{
+    // SpawnEx(Owner, Offset, SpawnTime, &GetRandomStream(Owner), ParticleBase);
+    SpawnEx(Owner, Offset, SpawnTime, nullptr, ParticleBase);
+}
+
+void UParticleModuleVelocity::SpawnEx(
+    FParticleEmitterInstance* Owner, int32 Offset, float SpawnTime, FRandomStream* InRandomStream, FBaseParticle* ParticleBase
+)
+{
+    SPAWN_INIT
+    {
+        FVector Vel = StartVelocity.GetValue(Owner->EmitterTime, Owner->Component, 0, InRandomStream);
+        FVector FromOrigin = (Particle.Location - Owner->EmitterToSimulation.GetOrigin()).GetSafeNormal();
+
+        FVector OwnerScale(1.0f);
+        if ((bApplyOwnerScale == true) && Owner->Component)
+        {
+            OwnerScale = Owner->Component->GetComponentTransform().GetScale3D();
+        }
+
+        UParticleLODLevel* LODLevel = Owner->SpriteTemplate->GetCurrentLODLevel(Owner);
+        assert(LODLevel);
+        if (LODLevel->RequiredModule->bUseLocalSpace)
+        {
+            if (bInWorldSpace == true)
+            {
+                Vel = Owner->SimulationToWorld.InverseTransformVector(Vel);
+            }
+            else
+            {
+                Vel = Owner->EmitterToSimulation.TransformVector(Vel);
+            }
+        }
+        else if (bInWorldSpace == false)
+        {
+            Vel = Owner->EmitterToSimulation.TransformVector(Vel);
+        }
+        Vel *= OwnerScale;
+        Vel += FromOrigin * StartVelocityRadial.GetValue(Owner->EmitterTime, Owner->Component, InRandomStream) * OwnerScale;
+        Particle.Velocity += Vel;
+        Particle.BaseVelocity += Vel;
+    }
+}
---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Particles/ParticleModules/ParticleModuleVelocity.h
@@ -0,0 +1,58 @@
+﻿#pragma once
+#include "ParticleModuleVelocityBase.h"
+#include "Distributions/DistributionFloat.h"
+#include "Distributions/DistributionVector.h"
+
+
+class UParticleModuleVelocity : public UParticleModuleVelocityBase
+{
+    DECLARE_CLASS(UParticleModuleVelocity, UParticleModuleVelocityBase)
+
+public:
+    UParticleModuleVelocity();
+    virtual ~UParticleModuleVelocity() override = default;
+
+    /** 
+     *	The velocity to apply to a particle when it is spawned.
+     *	Value is retrieved using the EmitterTime of the emitter.
+     */
+    UPROPERTY(
+        EditAnywhere,
+        FRawDistributionVector, StartVelocity, {};
+    )
+
+    /** 
+     *	The velocity to apply to a particle along its radial direction.
+     *	Direction is determined by subtracting the location of the emitter from the particle location at spawn.
+     *	Value is retrieved using the EmitterTime of the emitter.
+     */
+    UPROPERTY(
+        EditAnywhere,
+        FRawDistributionFloat, StartVelocityRadial, {}
+    )
+
+public:
+    /** Initializes the default values for this property */
+    void InitializeDefaults();
+
+    //~ Begin UObject Interface
+#if WITH_EDITOR
+    // virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif // WITH_EDITOR
+    virtual void PostInitProperties() override;
+    //~ End UObject Interface
+
+    //~ Begin UParticleModule Interface
+    virtual void Spawn(FParticleEmitterInstance* Owner, int32 Offset, float SpawnTime, FBaseParticle* ParticleBase) override;
+    //~ Begin UParticleModule Interface
+
+    /**
+     *	Extended version of spawn, allows for using a random stream for distribution value retrieval
+     *
+     *	@param	Owner				The particle emitter instance that is spawning
+     *	@param	Offset				The offset to the modules payload data
+     *	@param	SpawnTime			The time of the spawn
+     *	@param	InRandomStream		The random stream to use for retrieving random values
+     */
+    void SpawnEx(FParticleEmitterInstance* Owner, int32 Offset, float SpawnTime, struct FRandomStream* InRandomStream, FBaseParticle* ParticleBase);
+};
