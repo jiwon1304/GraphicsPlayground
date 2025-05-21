7ec235790689ec136573d630af868da46c678b01
정재현 <ray1203@naver.com>
2025-05-22 00:28:13 +0900

    fix: ShowFlags에 Particles 항목 추가 및 렌더링 조건 수정

--- EngineSIU/EngineSIU/Engine/Source/Editor/PropertyEditor/ShowFlags.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Editor/PropertyEditor/ShowFlags.cpp
@@ -13,7 +13,7 @@ void ShowFlags::Draw(const std::shared_ptr<FEditorViewportClient>& ActiveViewpor
         ImGui::OpenPopup("ShowFlags");
     }
 
-    const char* Items[] = { "AABB", "Primitives", "BillBoardText", "UUID", "Fog", "LightWireframe", "LightWireframeSelectedOnly", "Shadow", "Collision", "CollisionSelectedOnly", "SkeletalMesh" };
+    const char* Items[] = { "AABB", "Primitives", "BillBoardText", "UUID", "Fog", "LightWireframe", "LightWireframeSelectedOnly", "Shadow", "Collision", "CollisionSelectedOnly", "SkeletalMesh","Particles" };
     const uint64 CurFlag = ActiveViewport->GetShowFlag();
 
     if (ImGui::BeginPopup("ShowFlags"))
@@ -31,6 +31,7 @@ void ShowFlags::Draw(const std::shared_ptr<FEditorViewportClient>& ActiveViewpor
             static_cast<bool>(CurFlag & EEngineShowFlags::SF_Collision),
             static_cast<bool>(CurFlag & EEngineShowFlags::SF_CollisionSelectedOnly),
             static_cast<bool>(CurFlag & EEngineShowFlags::SF_SkeletalMesh),
+            static_cast<bool>(CurFlag & EEngineShowFlags::SF_Particles),
         }; // 각 항목의 체크 상태 저장
 
         for (int i = 0; i < IM_ARRAYSIZE(Items); i++)
@@ -97,7 +98,10 @@ uint64 ShowFlags::ConvertSelectionToFlags(const bool Selected[])
     {
         Flags |= static_cast<uint64>(EEngineShowFlags::SF_SkeletalMesh);
     }
-
+    if (Selected[11])
+    {
+        Flags |= static_cast<uint64>(EEngineShowFlags::SF_Particles);
+    }
     return Flags;
 }
 
